38ef51ae8e452a29dfa6b31164492d0ea066c13d
Harry Seldon <harry.seldon.f@gmail.com>
2009-04-18 18:09:55 +0200

    Clean up

--- app/controllers/test_it_controller.rb
+++ app/controllers/test_it_controller.rb
@@ -709,60 +709,6 @@ class TestItController < ApplicationController
     chart.y_axis =  y ;
     chart.set_tooltip( tooltip );
     
-    render :text => chart.to_s
-   
-#      <?php
-#
-#include_once '../php-ofc-library/open-flash-chart.php';
-#
-#$title = new title( 'Stuff I\'m thinking about, '.date("D M d Y") );
-#$title->set_style( "{font-size: 20px; color: #F24062; text-align: center;}" );
-#
-#$bar_stack = new bar_stack();
-#
-#// set a cycle of 3 colours:
-#$bar_stack->set_colours( array( '#C4D318', '#50284A', '#7D7B6A' ) );
-#
-#// add 3 bars:
-#$bar_stack->append_stack( array( 2.5, 5, 2.5 ) );
-#
-#// add 4 bars, the fourth will be the same colour as the first:
-#$bar_stack->append_stack( array( 2.5, 5, 1.25, 1.25 ) );
-#
-#
-#$bar_stack->append_stack( array( 5, new bar_stack_value(5, '#ff0000') ) );
-#$bar_stack->append_stack( array( 2, 2, 2, 2, new bar_stack_value(2, '#ff00ff') ) );
-#
-#$bar_stack->set_keys(
-#    array(
-#        new bar_stack_key( '#C4D318', 'Kiting', 13 ),
-#        new bar_stack_key( '#50284A', 'Work', 13 ),
-#        new bar_stack_key( '#7D7B6A', 'Drinking', 13 ),
-#        new bar_stack_key( '#ff0000', 'XXX', 13 ),
-#        new bar_stack_key( '#ff00ff', 'What rhymes with purple? Nurple?', 13 ),
-#        )
-#    );
-#$bar_stack->set_tooltip( 'X label [#x_label#], Value [#val#]<br>Total [#total#]' );
-#
-#
-#
-#$y = new y_axis();
-#$y->set_range( 0, 14, 2 );
-#
-#$x = new x_axis();
-#$x->set_labels_from_array( array( 'Winter', 'Spring', 'Summer', 'Autmn' ) );
-#
-#$tooltip = new tooltip();
-#$tooltip->set_hover();
-#
-#$chart = new open_flash_chart();
-#$chart->set_title( $title );
-#$chart->add_element( $bar_stack );
-#$chart->set_x_axis( $x );
-#$chart->add_y_axis( $y );
-#$chart->set_tooltip( $tooltip );
-#
-#echo $chart->toPrettyString();  
+    render :text => chart.to_s   
     end
-
 end
